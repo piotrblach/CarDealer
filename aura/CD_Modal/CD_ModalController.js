f883b9c37fc3049e5ec6c7ab3931397450691582
Piotr Błach <piotr.blach@brietenet.com.pl>
2018-06-27 14:50:14 +0200

    modal controller cleanup

--- aura/CD_Modal/CD_ModalController.js
+++ aura/CD_Modal/CD_ModalController.js
@@ -1,6 +1,4 @@
-/**
- * Created by BRITENET on 10.05.2018.
- */
+
 ({
 	show : function(component, event, helper) {
 		component.set("v.visible", true);
@@ -8,12 +6,8 @@
 
 	hide : function(component, event, helper) {
 		component.set("v.visible", false);
-//		$A.get("e.force:closeQuickAction").fire();
 	},
 	closeModal : function(component, event, helper) {
 		component.set("v.visible", false);
-//		$A.get("e.force:closeQuickAction").fire();
-//		var closeModalEvent = component.getEvent("closeModalEvent");
-//		closeModalEvent.fire();
 	}
 })
