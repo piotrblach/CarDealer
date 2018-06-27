e4a855c6f3b584eaae103581bcbe29d3d3cb84ce
Piotr Błach <piotr.blach@brietenet.com.pl>
2018-06-27 14:46:09 +0200

    added prices to car detail component

--- aura/CD_CarDetail/CD_CarDetailController.js
+++ aura/CD_CarDetail/CD_CarDetailController.js
@@ -1,15 +1,26 @@
 
 ({
     onCarIdChanged: function(component, event, helper){
+        component.find("loadingSpinner").show();
         let envType = component.get("v.environmentType");
-        if(envType === 'Community'){
+        if(envType === 'Community' || envType === 'Standalone'){
             let sPageURL = decodeURIComponent(window.location.search.substring(1));
             let carId = sPageURL.substring(3);
             component.set("v.carId", carId);
+        }else{
+            if(envType === 'CommunityStandardPage'){
+                let sPageURL = decodeURIComponent(window.location.pathname);
+                let tempPath = sPageURL.substring(0, sPageURL.lastIndexOf('/'));
+                let carId = tempPath.substring(tempPath.lastIndexOf('/')+1);
+                component.set("v.carId", carId);
+            }
         }
         component.find("carDetail").reloadRecord();
+        helper.loadCarStandardPrice(component)
+        helper.loadCarLowestPrice(component)
         component.set("v.editorVisible", true);
         component.set("v.editorVisible", false);
+        component.find("loadingSpinner").hide();
     },
     setEditMode: function(component){
         component.set("v.editorVisible", true);
--- aura/CD_CarDetail/CD_CarDetailHelper.js
+++ aura/CD_CarDetail/CD_CarDetailHelper.js
@@ -40,5 +40,63 @@
             resultsToast.fire();
             component.find("removalConfirmationModal").hide();
         }));
+    },
+    loadCarStandardPrice: function(component){
+        let action = component.get('c.getCarStandardPrice');
+
+        action.setParams({
+            carId: component.get("v.carId")
+        })
+
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS")
+            {
+                component.set("v.carStandardPrice", response.getReturnValue());
+            }else{
+                let resultsToast = $A.get("e.force:showToast");
+                if ($A.util.isUndefined(resultsToast)){
+                    alert('Error when loading car price');
+                }else{
+                    resultsToast.setParams({
+                        "type": "error",
+                        "title": "Error",
+                        "message": response.getError()[0].message
+                    });
+                    resultsToast.fire();
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    loadCarLowestPrice: function(component){
+        let action = component.get('c.getCarLowestPrice');
+
+        action.setParams({
+            carId: component.get("v.carId")
+        })
+
+        action.setCallback(this, function(response){
+            let state = response.getState();
+            if (state === "SUCCESS")
+            {
+                let carLowestPrice = component.get("v.carLowestPrice");
+                carLowestPrice = response.getReturnValue();
+                component.set("v.carLowestPrice", carLowestPrice);
+            }else{
+                let resultsToast = $A.get("e.force:showToast");
+                if ($A.util.isUndefined(resultsToast)){
+                    alert('Error when loading car price');
+                }else{
+                    resultsToast.setParams({
+                        "type": "error",
+                        "title": "Error",
+                        "message": response.getError()[0].message
+                    });
+                    resultsToast.fire();
+                }
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
